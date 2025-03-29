44b7f4b157c4fee8c6b93049eb5b7b733e2d7eb1
harsh sharma <165451996+harshdev2909@users.noreply.github.com>
2025-03-29 22:16:18 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import Menu from './Menu'
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Course from './components/Course'
 const App = () => {
   return (
-    <div className='bg-gradient-to-r from-violet-500 to-purple-500 h-full p-2 '>
-      <Menu/>
+    <div className='bg-gradient-to-r from-violet-500 to-purple-500 h-screen p-2 '>
+      <Navbar/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/course' element={<Course/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
